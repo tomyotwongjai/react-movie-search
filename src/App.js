7bfb28821c2b3c37b0bde18cd0b63e1da1813a21
Tom Yotwongjai <munch2018@gmail.com>
2022-07-02 15:59:52 +0200

    Add app file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
+import {MovieDetail, PageNotFound, Footer, Header, Home} from './components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          {/* <Route exact path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*" element={<PageNotFound />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
