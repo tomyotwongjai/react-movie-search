f6ec844bb7c04a8642040236fbeedefdd36d4b67
Tom Yotwongjai <munch2018@gmail.com>
2022-07-02 17:13:13 +0200

    styling header & added route

---
+++ src/common/apis/MovieApikey.js
@@ -0,0 +1,1 @@
+export const api = "2fbe92af"
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export {default as Header} from './Header/Header';
-export {default as Footer} from './Footer/Footer';
-export {default as PageNotFound} from './PageNotFound/PageNotFound';
-export {default as MovieDetail} from './MovieDetail/MovieDetail';
+export {default as Header} from './Header/Header'
+export {default as Footer} from './Footer/Footer'
+export {default as PageNotFound} from './PageNotFound/PageNotFound'
+export {default as MovieDetail} from './MovieDetail/MovieDetail'
+export {default as Home} from './Home/Home'
